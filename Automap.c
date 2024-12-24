7c79a6f5c006c64057d02c6b393ff771bec6ed9d
desmond.obrien <des@Dess-MacBook-Air.local>
2024-12-24 15:53:09 -0700

    switching to other work

--- Automap.c
+++ Automap.c
@@ -10,8 +10,8 @@ void AUTOMAP_render_map(SDL_Window* wind, SDL_Surface* surface, MAP map, RVECTOR
 
 
     AUTOMAP_render_player(surface, player_pos);
-    //AUTOMAP_render_walls(surface, walls, map.h * map.w, player_pos);
-    AUTOMAP_render_vector(surface, player_pos);
+    AUTOMAP_render_walls(surface, map.map, map.h * map.w, player_pos);
+    //AUTOMAP_render_vector(surface, player_pos);
 
    // SDL_UpdateWindowSurface(wind);
     SDL_UnlockSurface(surface);
@@ -36,8 +36,8 @@ void AUTOMAP_render_player(SDL_Surface* surface, RVECTOR player_pos) {
 
 void AUTOMAP_render_wall(SDL_Surface* surface, WALL wall, RVECTOR player_pos) {
     uint32_t* pixels = surface->pixels;
-    uint32_t x_off = wall.x_pos;// - player_pos.head.x;// - AUTOMAP_get_draw_offset_x(player_pos);
-    uint32_t z_off = wall.z_pos;// - player_pos.head.z;// - AUTOMAP_get_draw_offset_z(player_pos);
+    uint32_t x_off = wall.x_pos - player_pos.head.x;// - AUTOMAP_get_draw_offset_x(player_pos);
+    uint32_t z_off = wall.z_pos - player_pos.head.z;// - AUTOMAP_get_draw_offset_z(player_pos);
 
     SDL_LockSurface(surface);
     for (int i = z_off; i < z_off + BLOCK_SIZE; ++i) {
--- makefile
+++ makefile
@@ -1,3 +1,3 @@
 compileshit:
-	gcc *.c -o main `sdl2-config --libs --cflags` -ggdb3 -g -O0 --std=c17 -Wall -F/Library/Frameworks -framework SDL2 -framework SDL2_image -lm
-	
+	gcc *.c -o main `sdl2-config --libs --cflags` -ggdb3 -O0 --std=c17 -Wall -F/Library/Frameworks -framework SDL2 -framework SDL2_image -lm
+	./main
